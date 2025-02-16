a4242705250dbc144df404282745826bad6eba9b
Premkumarjp012 <22kf5a0502@sseptp.org>
2025-02-16 23:03:33 +0530

    4th commit

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-/*
+
 import './config/instrument.js'
 import express from 'express'
 import cors from 'cors'
@@ -46,39 +46,5 @@ Sentry.setupExpressErrorHandler(app);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-})*/
-
-import './config/instrument.js'
-import express from 'express';
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from "./config/db.js";
-import * as Sentry from "@sentry/node";
-
-
-
-//Initialize Express
-
-const app=express()
-
-//connect to database
-
-await connectDB()
-
-//middlewares
-app.use(cors())
-app.use(express.json())
-
-//Routes
-app.get('/',(req,res) => res.send("Api Working"))
-app.get("/debug-sentry", function mainHandler(req, res) {
-  throw new Error("My first Sentry error!");
-});
-
-//port
-const PORT= process.env.PORT || 5010
-Sentry.setupExpressErrorHandler(app);
-
-app.listen(PORT,() =>{
-  console.log(`Server is Running on Port ${PORT} `)
 })
+
